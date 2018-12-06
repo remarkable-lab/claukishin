23c54c7142da926cb3341e66bd52bfb9ee9463cf
Hans <hans@wisy.app>
2018-12-06 17:48:03 -0500

    feat: nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function Nav(props){
+    return (
+        <nav>
+            <ul>
+                <li>Nav</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
