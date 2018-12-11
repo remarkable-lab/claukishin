bcd1285614e98b7d4c39b3980c559b98a2207f22
Hans <hans@wisy.app>
2018-12-10 23:05:40 -0500

    gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-emotion',
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+        sourceMap: true,
+        autoLabel: process.env.NODE_ENV !== 'production',
+        labelFormat: '[local]',
+        cssPropOptimization: true,
+      },
+    },
+  ],
+};
