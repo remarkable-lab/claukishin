ee99a8c2445b559df01b17e447b377b29522e42e
Hans <hans@wisy.app>
2018-12-10 23:05:59 -0500

    Link from index to contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-import React from "react"
-import Layout from "../components/layout";
+
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-    <Layout>
-        <div>Index</div>
-    </Layout>
-)
+  <Layout>
+    <div>Index</div>
+    <Link to="/contacto">Contacto</Link>
+  </Layout>
+);
