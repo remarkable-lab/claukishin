04147054b38250fce77efd0ffb7fa4e95a461d25
Hans <hans@wisy.app>
2018-12-10 23:05:27 -0500

    contact page

---
+++ src/pages/contacto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout>
+    <div>Contacto</div>
+    <Link to="/">Casa</Link>
+  </Layout>
+);
