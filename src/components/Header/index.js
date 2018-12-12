57312f51c5b5a1b590a4778f7b8a844c97b87d4c
Hans <hans@wisy.app>
2018-12-12 17:47:07 -0500

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <div>
+            <h1>Title</h1>
+            <p>Subtitle</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
