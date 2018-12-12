153173b0d3294c1e6fd2b190bcd019716849643c
Hans <hans@wisy.app>
2018-12-12 17:21:05 -0500

    basic footer structure

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,60 @@
 import React from "react";
-import { Link } from "gatsby";
+import { css } from "@emotion/core";
+import instaIcon from "../../assets/instagram.svg";
+
+const container = css`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  div {
+    display: flex;
+  }
+  @media (min-width: 620px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
 
 function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer
+      css={css`
+        background-color: white;
+        border-top: 1px solid black;
+      `}
+    >
+      <div css={container}>
+        <div>
+          Sigueme en
+          <ul>
+            <li>
+              <a
+                href="https://www.instagram.com/hans.lebon/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ marginLeft: "5px" }}
+              >
+                <img
+                  src={instaIcon}
+                  alt="instagram page"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    maxWidth: "18px",
+                    margin: "auto",
+                    borderRadius: "7px"
+                  }}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          Claudia Flores
+          <span style={{ color: "gray" }}>|</span>
+          Finanzas
+        </div>
+      </div>
     </footer>
   );
 }
