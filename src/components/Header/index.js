8604c5704b06aab2464482fb90c9ed2a54bf04f3
Hans <hans@wisy.app>
2018-12-12 17:56:35 -0500

    display title and subtitle using static query and graphql

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,36 @@
 import React from "react";
+import { StaticQuery, graphql } from "gatsby";
 
 function Header() {
   return (
-    <header>
-      <div>
-        <div>
-          <div>
-            <h1>Title</h1>
-            <p>Subtitle</p>
-          </div>
-        </div>
-      </div>
-    </header>
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+              subtitle
+            }
+          }
+        }
+      `}
+      render={site => {
+        const { siteMetadata } = site;
+        const { title, subtitle } = siteMetadata;
+        return (
+          <header>
+            <div>
+              <div>
+                <div>
+                  <h1>{title}</h1>
+                  <p>{subtitle}</p>
+                </div>
+              </div>
+            </div>
+          </header>
+        );
+      }}
+    />
   );
 }
 
