7c99a29b974a0c233a9a3a4bde5422f1b59f987c
Hans <hans@wisy.app>
2018-12-11 21:48:53 -0500

    chore: prettier config with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,40 @@
 module.exports = {
-  extends: "airbnb",
+  extends: ["airbnb", "prettier"],
   parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 8, // optional, recommended 6+
+    ecmaFeatures: {
+      "experimentalObjectRestSpread": true,
+      "impliedStrict": true,
+      "classes": true
+    }
   },
   env: {
     "browser": true,
     "node": true,
   },
+  plugins: ["prettier"],
   rules: {
+    "prettier/prettier": "error",
     "react/jsx-filename-extension": [
       1,
       {
         "extensions": [".js", ".jsx"]
       }
     ],
+    "prefer-const": [
+      "error",
+      {
+        "destructuring": "all",
+      }
+    ],
+    "arrow-body-style": [
+      2,
+      "as-needed"
+    ],
+    "no-console": 0,
+    "import/prefer-default-export": 0,
+    "import": 0,
+    "space-before-function-paren": 0,
   }
 }
