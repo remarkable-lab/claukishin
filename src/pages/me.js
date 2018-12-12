81b7672cd32a1c3dd8717ed94935905bafd1ed15
Hans <hans@wisy.app>
2018-12-11 23:36:18 -0500

    about me page

---
+++ src/pages/me.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <div>Sobre me</div>
+    <Link to="/">Casa</Link>
+  </Layout>
+);
