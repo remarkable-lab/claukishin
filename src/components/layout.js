bc08a4b1b58996d80327af00e2cfe4d86f32d8a3
Hans <hans@wisy.app>
2018-12-11 23:36:03 -0500

    prettier format

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,23 @@
 import React from "react";
-import Nav from "../components/Nav"
+import Nav from "./Nav";
 import "../styles/app.css";
 import "../styles/reboot.css";
 
-function Layout(props){
-    return (
-        <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-            <div style={{ flex: "1 0 auto" }}>
-                <div>
-                    <Nav/>
-                    {props.children}
-                </div>
-            </div>
-            <div style={{ flexShrink: 0 }}>
-                <h6>Footer</h6>
-            </div>
+function Layout(props) {
+  const { children } = props;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <div style={{ flex: "1 0 auto" }}>
+        <div>
+          <Nav />
+          {children}
+        </div>
       </div>
-    )
+      <div style={{ flexShrink: 0 }}>
+        <h6>Footer</h6>
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
