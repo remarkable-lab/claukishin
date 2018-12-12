674c883bab67f014075a4210d3bfee0186ece123
Hans <hans@wisy.app>
2018-12-11 23:35:01 -0500

    prettier format

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
 module.exports = {
+  pathPrefix: `/`,
   plugins: [
     {
-      resolve: 'gatsby-plugin-emotion',
+      resolve: "gatsby-plugin-emotion",
       options: {
         // Accepts all options defined by `babel-plugin-emotion` plugin.
         sourceMap: true,
-        autoLabel: process.env.NODE_ENV !== 'production',
-        labelFormat: '[local]',
-        cssPropOptimization: true,
-      },
-    },
-  ],
+        autoLabel: process.env.NODE_ENV !== "production",
+        labelFormat: "[local]",
+        cssPropOptimization: true
+      }
+    }
+  ]
 };
