cd6182ad8263bf56dd33bb56feec2fb5786f27a7
Hans <hans@wisy.app>
2018-12-12 21:29:56 -0500

    add header to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import Header from "../components/Header";
 
 export default () => (
   <Layout>
+    <Header />
     <div>Index</div>
     <Link to="/contacto">Contacto</Link>
   </Layout>
