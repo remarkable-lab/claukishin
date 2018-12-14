7f5218c104621add74a276f4f9726ec33c189f00
Hans <hans@wisy.app>
2018-12-14 06:06:02 -0500

    aside component

---
+++ src/components/Aside/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Aside() {
+  return (
+    <aside>
+      <section>
+        <article>
+          <header>
+            <h3>Hans</h3>
+            <p>Hola mi nombre es Hans, bienvenido a mi blog</p>
+          </header>
+        </article>
+      </section>
+      <section>
+        <h3>Tags</h3>
+      </section>
+    </aside>
+  );
+}
+
+export default Aside;
