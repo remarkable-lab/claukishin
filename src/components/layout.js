4fb1e3994ab05cd3b1dab0eac4c3d09f77bdecc6
Hans <hans@wisy.app>
2018-12-17 22:15:03 -0500

    using location with Reach

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
+import { Location } from "@reach/router";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import "../styles/app.css";
@@ -8,17 +9,23 @@ import "../styles/reboot.css";
 export default function Layout(props) {
   const { children } = props;
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <div style={{ flex: "1 0 auto" }}>
-        <div>
-          <Nav />
-          {children}
+    <Location>
+      {({ location }) => (
+        <div
+          style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+        >
+          <div style={{ flex: "1 0 auto" }}>
+            <div>
+              <Nav location={location} />
+              {children}
+            </div>
+          </div>
+          <div style={{ flexShrink: 0 }}>
+            <Footer />
+          </div>
         </div>
-      </div>
-      <div style={{ flexShrink: 0 }}>
-        <Footer />
-      </div>
-    </div>
+      )}
+    </Location>
   );
 }
 
