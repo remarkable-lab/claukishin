7fbcfefa287df5161d3402f9a2f0f50c28900d66
Hans <hans@wisy.app>
2018-12-27 21:48:32 -0500

    delete old file path

--- src/helper/index.js
+++
@@ -1,9 +0,0 @@
-export const getDate = (date, format = {}) => {
-  const { y = "numeric", m = "numeric", d = "numeric" } = format;
-  const newDate = new Date(date).toLocaleDateString("es-ES", {
-    year: y,
-    month: m,
-    day: d
-  });
-  return newDate;
-};
