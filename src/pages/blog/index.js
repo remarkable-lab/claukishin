9432274e57a16da3eb9cc13af84d0eea9d048c48
Hans <hans@wisy.app>
2018-12-28 06:34:00 -0500

    new post index page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,60 @@
 import React from "react";
-import { Link } from "gatsby";
+import { graphql, StaticQuery } from "gatsby";
+import SEO from "../../components/Seo";
+import Posts from "../../components/Posts";
 import Layout, { Content } from "../../components/layout";
+
+const queryPosts = graphql`
+  query {
+    allMarkdownRemark(
+      filter: { frontmatter: { public: { eq: true } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            date
+            tags
+            public
+            author
+          }
+          excerpt
+          timeToRead
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default () => (
   <Layout>
+    <SEO
+      title="Mis artículos"
+      keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+    />
     <Content>
-      <div>Blog index</div>
-      <Link to="/">Casa</Link>
+      <section>
+        <h1
+          style={{
+            marginTop: "1rem",
+            marginBottom: "1.5rem"
+          }}
+        >
+          Artículos
+        </h1>
+        <StaticQuery
+          query={queryPosts}
+          render={({ allMarkdownRemark }) => {
+            const { edges } = allMarkdownRemark;
+            return <Posts posts={edges} />;
+          }}
+        />
+      </section>
     </Content>
   </Layout>
 );
